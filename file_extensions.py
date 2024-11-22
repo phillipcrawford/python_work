606ebea13cb6576ed904ee9c7b988697a1cfba24
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-22 14:26:50 -0800

    add comments to 2.8, file extentions

--- file_extensions.py
+++ file_extensions.py
@@ -1,3 +1,5 @@
+# By Phillip Crawford
+# program demonstrates how to use removesuffix method
 filename = 'python_notes.txt'
 modified_filename = filename.removesuffix(".txt")
 print(modified_filename)
