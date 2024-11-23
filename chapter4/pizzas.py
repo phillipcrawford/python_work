924f3219a91d71d081b35c8e43ebdbf658d8cb12
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-23 02:10:07 -0800

    add exercise 4.1, pizzas

---
+++ chapter4/pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ["pepperoni", "cheese", "neapolitan"]
+for pizza in pizzas:
+    print(f"Here's a type of pizza: {pizza.title()}")
+print("\nUnpopular opinion, I don't care for pizza. The ratios are all wrong. Lots of bread, lots of cheese, not much else")
