81f580bf5523dd486cf01d17029ff6cc5bb6be6c
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-22 19:01:59 -0800

    add exercise 3.4, guest list

---
+++ chapter3/guest_list.py
@@ -0,0 +1,4 @@
+guest_list = ['dad', 'grandpa', 'grandma']
+print(f"{guest_list[0].title()}, you are invited to dinner!")
+print(f"So are you {guest_list[1].title()}")
+print(f"I wouldn't forget {guest_list[2].title()}, so much fun!")
