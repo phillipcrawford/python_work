72008fbaeb980193ce7b54561940e91a98772a61
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-23 16:39:51 -0800

    add exercise 4.13, buffet

---
+++ chapter4/buffet.py
@@ -0,0 +1,8 @@
+foods = ("rice", "naan", "palaak paneer", "chicken vindaloo", "chicken tikka masala")
+for food in foods:
+    print(food)
+#foods[1] = "garbage"
+print("\n")
+foods = ("rice", "papadam", "saag panner", "chicken vindaloo", "chicken tikka masala")
+for food in foods:
+    print(food)
