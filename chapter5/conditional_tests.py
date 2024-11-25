2b510059bc7c089abc8231f8d97c63c1a3732647
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-24 17:44:12 -0800

    add exercise 5.1-5.2, conditional tests

---
+++ chapter5/conditional_tests.py
@@ -0,0 +1,22 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car == 'Subaru'? I predict False.")
+print(car == 'Subaru')
+
+print("\nIs car with title method called == 'Subaru'? I predict True.")
+print(car.title() == 'Subaru')
+
+value = 17
+print("\nIs value == '17'? I predict True.")
+print(value == 17)
+
+print("\nIs value == '18'? I predict True.")
+print(value == 18)
+
+print("\nIs car == 'subaru' AND value == '17'? I predict True.")
+print(car == 'subaru' and value == 17)
