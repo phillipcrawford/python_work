ad10f43fb78b9e973a3227d1fa307b3945f8d3b7
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-24 21:31:28 -0800

    add exercise 5.5, alien colors3

---
+++ chapter5/alien_colors3.py
@@ -0,0 +1,7 @@
+alien_color = 'red'
+if alien_color == 'green':
+    print("you've scored 5 points")
+elif alien_color == 'yellow':
+    print("you've scored 10 points")
+elif alien_color == 'red':
+    print("you've scored 15 points")
