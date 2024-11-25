a6cfa2fa90054890c5ac3ad206a99644de8c8096
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-24 21:35:43 -0800

    add exercise 5.6, stages of life

---
+++ chapter5/stages_of_life.py
@@ -0,0 +1,13 @@
+age = 100
+if age < 2:
+    print("this person is a baby")
+elif age < 4:
+    print("this person is a toddler")
+elif age < 13:
+    print("this person is kid")
+elif age < 20:
+    print("this person is a teenager")
+elif age < 65:
+    print("this person is an adult")
+else:
+    print("this person is an elder")
