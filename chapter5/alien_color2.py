07e599ccd88c18bf8204450c745fad23ed0480ba
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-24 21:24:50 -0800

    add exercise 5.4, alien colors2

---
+++ chapter5/alien_color2.py
@@ -0,0 +1,5 @@
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("you've earned 5 points")
+else:
+    print('you\'ve earned 10 points')
--- chapter5/notes.py
+++ chapter5/notes.py
@@ -5,3 +5,5 @@
 # <, <=, >, >= : comparisons
 # in : contains
 # not : not contain 
+
+#it's ok to omit 'else' in elif blocks
