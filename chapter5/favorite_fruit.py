90ae98cdacaf0ffbf15adf6e472dbbccc815b6cd
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-24 21:41:13 -0800

    add exercise 5.7, favorite fruit

---
+++ chapter5/favorite_fruit.py
@@ -0,0 +1,11 @@
+favorite_fruits = ["apple", "banana", "cherry"]
+if 'apple' in favorite_fruits:
+    print("you really like apples")
+if 'banana' in favorite_fruits:
+    print("you really like banana")
+if 'cherry' in favorite_fruits:
+    print("you really like cherry")
+if 'pineapple' in favorite_fruits:
+    print("you really like pineapple")
+if 'hucklyberry' in favorite_fruits:
+    print("you really like huckleberry")
