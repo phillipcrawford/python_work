e47a1419bc807bcce2b26c37e6f703da219f8a33
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-25 22:36:04 -0800

    add exercise 6.4, glossary2

---
+++ chapter6/glossary2.py
@@ -0,0 +1,6 @@
+glossary = {"in": "contains", "not": "does not contain"}
+#print(glossary['in'])
+#print(glossary['not'])
+for k, v in glossary.items():
+    print(f"key is '{k}'")
+    print(f"value is '{v}'")
