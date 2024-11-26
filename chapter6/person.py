461cb485761f0f97d78ae16d635914ddfdb6db7f
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-25 20:16:08 -0800

    add exercise 6.1, person

---
+++ chapter6/person.py
@@ -0,0 +1,5 @@
+phil = {'first_name': 'phil', 'last_name': 'crawford', 'age': 38, 'city': 'Bozeman'}
+print(phil['first_name'].title())
+print(phil['last_name'].title())
+print(phil['age'])
+print(phil['city'])
