5fd45be5089892ebe5f49ce23179bf40303cefb1
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-25 20:21:42 -0800

    add exercise 6.2, favorite numbers

---
+++ chapter6/favorite_numbers.py
@@ -0,0 +1,9 @@
+favorite_numbers = {
+    'phil': 16777216, 
+    'bob': 27, 
+    'paul':47, 
+    'koopman': 200, 
+    'albert': 49
+    }
+print(f"Phil's favorite number is {favorite_numbers['phil']}")
+print(f"Bob's favorite number is {favorite_numbers['bob']}")
