8eb55c34247b1088d83f7849ff3b007d962493ac
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-28 01:07:17 -0800

    add exercise 8.4, large shirts

---
+++ chapter8/large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size='large', text='I love Python.'):
+    print(f"This t-shirt is of size {size} and it says '{text}' on it.")
+
+make_shirt()
+make_shirt('medium')
+make_shirt(text='boom boom boom',size='medium')
