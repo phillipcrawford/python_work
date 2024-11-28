561b56649a70941e51694d5f2e0fbbb4bf2b29f5
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-27 21:24:55 -0800

    add exercise 7.8, deli

---
+++ chapter7/deli.py
@@ -0,0 +1,8 @@
+sandwich_orders = ["pesto", "cheese", "turkey and cheese"]
+finished_sandwiches = []
+
+for sandwich in sandwich_orders:
+    print(f"I made your {sandwich} sandwich")
+    finished_sandwiches.append(sandwich)
+
+print(finished_sandwiches)
