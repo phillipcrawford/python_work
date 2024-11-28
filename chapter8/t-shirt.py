c6a4db1302e795b74bf5d4b395c684b1355530c9
Phillip Crawford <phillip.crawford3@gmail.com>
2024-11-28 01:03:12 -0800

    add exercise 8.3, t-shirt

---
+++ chapter8/t-shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, text):
+    print(f"This t-shirt is of size {size} and it says '{text}' on it.")
+
+make_shirt('large', 'hello, here i am')
+make_shirt(text='boom boom boom',size='medium')
