04c2f2985a3ec716f18d85bbe616b3c8c67414ee
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-01 00:17:51 -0800

    upgrade ElectricCar class to be ready for exercise 9.9

--- chapter9/battery_upgrade.py
+++ chapter9/battery_upgrade.py
@@ -26,7 +26,25 @@ class ElectricCar(Car):
     
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.battery_size = 40
+        self.battery = Battery()
+
+class Battery:
+    
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
 
     def describe_battery(self):
-        print(f"This car has a {self.battery_size}-kWh battery")
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
