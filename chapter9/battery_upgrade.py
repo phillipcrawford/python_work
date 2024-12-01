4c3a18ac1192492084bfa58d397ac87040e96192
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-01 00:03:32 -0800

    add new attributes and method to child class ElectricCar

--- chapter9/battery_upgrade.py
+++ chapter9/battery_upgrade.py
@@ -23,5 +23,10 @@ class Car:
         self.odometer_reading += mileage
 
 class ElectricCar(Car):
+    
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery")
