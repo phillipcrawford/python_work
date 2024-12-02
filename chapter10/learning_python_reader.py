ef7ab84db0390e205030f8691feedfd2c56e259b
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-01 23:54:37 -0800

    complete exercise 10.1

--- chapter10/learning_python_reader.py
+++ chapter10/learning_python_reader.py
@@ -3,6 +3,7 @@ from pathlib import Path
 path = Path('python_reader.txt')
 contents = path.read_text()
 print(contents)
-#lines = contents.splitlines()
-#for line in lines:
-#    print(line)
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
