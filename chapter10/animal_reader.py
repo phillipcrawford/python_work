1eed97211111c167d2bb262789e82e07af004f12
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-02 08:54:14 -0800

    complete exercise 10.8, handle FileNotFound errors

--- chapter10/animal_reader.py
+++ chapter10/animal_reader.py
@@ -3,11 +3,14 @@ from pathlib import Path
 animals = ['cats.txt', 'dogs.txt']
 for animal in animals:
     path = Path(animal)
-    contents = path.read_text()
-
-    lines = contents.splitlines()
-    for line in lines:
-        print(line)
+    try:
+        contents = path.read_text()
+    except FileNotFoundError:
+        print(f"Sorry, the file, {path} does not exist.")
+    else:
+        lines = contents.splitlines()
+        for line in lines:
+            print(line)
 
 
 
