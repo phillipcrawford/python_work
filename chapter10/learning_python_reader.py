7193ed9ebf2d461a9fe6f82f44b349602ab704ce
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-01 23:49:21 -0800

    identify error: program not supported in VScode, must run in terminal

--- chapter10/learning_python_reader.py
+++ chapter10/learning_python_reader.py
@@ -1,2 +1,8 @@
 from pathlib import Path
-path = Path(learning_python.txt)
+
+path = Path('python_reader.txt')
+contents = path.read_text()
+print(contents)
+#lines = contents.splitlines()
+#for line in lines:
+#    print(line)
