fcbd85a6f3d697e996d6ff2e012dcf8be543a273
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-02 08:16:55 -0800

    add exercise 10.7, prompt repeated if previous input invalid

---
+++ chapter10/addition_calculator.py
@@ -0,0 +1,10 @@
+while True:
+    first_number = input('Please enter your first number: ')
+    second_number = input('Please enter your second number: ')
+    try:
+        sum = int(first_number) + int(second_number)
+    except ValueError:
+        print("invalid input, only numbers will work")
+    else:
+        print(sum)
+        break
