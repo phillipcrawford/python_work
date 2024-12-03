4a4a73082c66d1874a4c72c7e52623be94ff5fc7
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-03 05:01:15 -0800

    add exercise 10.14, refactor for composition, confirm returning user name before read

--- chapter10/remember_me.py
+++ chapter10/remember_me.py
@@ -3,17 +3,26 @@ import json
 
 path = Path('remember_me.json')
 
-if path.exists():
-    #read
-    contents = path.read_text()
-    lines = json.loads(contents)
-    for key, values in lines.items():
-        print(f'Your {key} is {values}')
-else:
-    #write
+def write_user():
     contents = {}
     contents['first name'] = input("What is your first name? ")
     contents['last name'] = input("what is your last name? ")
     contents['age'] = input("What is your age? ")
     contents = json.dumps(contents)
     path.write_text(contents)
+
+def read_user():
+    contents = path.read_text()
+    lines = json.loads(contents)
+    prompt = input(f'Are you {lines["first name"]}? Type \'yes\' to confirm ')
+    if prompt == 'yes':
+        for key, values in lines.items():
+            print(f'Your {key} is {values}')
+    else:
+        write_user()
+def greet_user():
+    if path.exists():
+        read_user()
+    else:
+        write_user()
+greet_user()
