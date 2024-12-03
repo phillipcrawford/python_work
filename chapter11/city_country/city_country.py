d85c00bfda64a40b427f0fe565e59f3d43b5bc40
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-03 09:43:36 -0800

    restructure files into folders to split two projects, make working function and driver for second project

---
+++ chapter11/city_country/city_country.py
@@ -0,0 +1,4 @@
+from city_country_function import city_country
+
+location = city_country('santiago', 'chile')
+print(location)
---
+++ chapter11/city_country/city_country_function.py
@@ -0,0 +1,4 @@
+def city_country(city, country):
+    """Generate a neatly formatted location name"""
+    location = f"{city}, {country}"
+    return location.title()    
