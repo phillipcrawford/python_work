81453644304879384adf95f663d77023d24669c8
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-04 14:04:33 -0800

    refactor to add helper methods _check_events() and _update_screen()

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -23,18 +23,27 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._update_screen()
 
             # Make the most reactly drawn screen visible.
-            pygame.display.flip()
             self.clock.tick(60)
 
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+                 
+        pygame.display.flip()
+                    
 if __name__ == '__main__':
     # Make a game instance and run the game.
     ai = AlienInvasion()
