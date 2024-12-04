685f810c2df4c89d13943bb1becb2dd863ba6fa2
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-04 03:45:44 -0800

    add second test

--- chapter11/employee/test_employee.py
+++ chapter11/employee/test_employee.py
@@ -4,3 +4,8 @@ def test_give_default_raise():
     billy = Employee('billy', 'joe', 50000)
     billy.give_raise()
     assert billy.salary == 55000
+
+def test_give_custom_raise():
+    bobby = Employee('bobby', 'bogen', 54000)
+    bobby.give_raise(6000)
+    assert bobby.salary == 60000
