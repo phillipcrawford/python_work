0f4915dcb46b7a2ac660a7faf6747abbbfed3a4f
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-04 03:54:55 -0800

    refactor tests to use fixture

--- chapter11/employee/test_employee.py
+++ chapter11/employee/test_employee.py
@@ -1,11 +1,16 @@
+import pytest
 from employee import Employee
 
-def test_give_default_raise():
-    billy = Employee('billy', 'joe', 50000)
-    billy.give_raise()
-    assert billy.salary == 55000
+@pytest.fixture
+def billy_employee():
+    """An employee that will be available to all test functions"""
+    billy_employee = Employee('billy', 'joe', 50000)
+    return billy_employee
 
-def test_give_custom_raise():
-    bobby = Employee('bobby', 'bogen', 54000)
-    bobby.give_raise(6000)
-    assert bobby.salary == 60000
+def test_give_default_raise(billy_employee):
+    billy_employee.give_raise()
+    assert billy_employee.salary == 55000
+
+def test_give_custom_raise(billy_employee):
+    billy_employee.give_raise(6000)
+    assert billy_employee.salary == 56000
