25485ed364a034605c14106289a0b24795bafdfc
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-05 04:23:26 -0800

    refactor keydown event into helper method

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -41,15 +41,19 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True        
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
