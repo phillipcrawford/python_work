42504322da24eadafad69d8a6748092857c46a15
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-05 14:25:33 -0800

    add move up and down ability to rocket class

--- mid_rocket/rocket.py
+++ mid_rocket/rocket.py
@@ -18,10 +18,13 @@ class Rocket:
 
         # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         # Movement flag; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on the movement flags."""
@@ -29,9 +32,14 @@ class Rocket:
             self.x += self.settings.rocket_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.rocket_speed
+        if self.moving_up:
+            self.y += self.settings.rocket_speed
+        if self.moving_down:
+            self.y -= self.settings.rocket_speed
 
         # Update rect object from self.x.
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
