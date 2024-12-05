318bdbc318f5961915c21112106d80da46998458
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-04 16:02:46 -0800

    add move left functionality

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -42,12 +42,15 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
                     self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-                    
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                                        
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
