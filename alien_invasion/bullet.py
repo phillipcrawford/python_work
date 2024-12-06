7c36a65c6da10239436d135ad017cb5643c177f7
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-06 05:03:05 -0800

    add attributes to settings and bullet init methods

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -1,6 +1,11 @@
 import pygame
-from pygame.sprite import Sprite
+from pygame.sprite import _Group, Sprite
 
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
     
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
