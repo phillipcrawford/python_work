fc8000b9b3a539f0d677603bee8225f3f06a55c2
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-06 06:20:56 -0800

    add a comment which poses question about deleting bullets

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -29,3 +29,6 @@ class Bullet(Sprite):
         def draw_bullet(self):
             """Draw the bullet to the screen"""
             pygame.draw.rect(self.screen, self.color, self.rect)
+
+            # Do we ever delete bullets? 
+            # Keep memory from being clogged with 100_000 bullets?
