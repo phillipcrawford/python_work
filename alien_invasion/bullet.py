0454bc30636bea5e2380fff2dd9d7541e36ffadd
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-06 07:09:56 -0800

    make fire bullets method

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -19,16 +19,16 @@ class Bullet(Sprite):
         # Store the bullet's position as a float
         self.y = float(self.rect.y)
 
-        def update(self):
-            """Move the bullet up the screen."""
-            # Update the exact position of the bullet.
-            self.y -= self.settings.bullet_speed
-            # Update the rect position.
-            self.rect.y = self.y
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the exact position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
 
-        def draw_bullet(self):
-            """Draw the bullet to the screen"""
-            pygame.draw.rect(self.screen, self.color, self.rect)
+    def draw_bullet(self):
+        """Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
 
-            # Do we ever delete bullets? 
-            # Keep memory from being clogged with 100_000 bullets?
+    # Do we ever delete bullets? 
+    # Keep memory from being clogged with 100_000 bullets?
