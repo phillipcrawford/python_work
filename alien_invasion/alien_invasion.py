3a18bc26041a89cb01a91e74bdf46799ec382079
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-07 05:55:38 -0800

    build a fleet rather than one alien

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -87,9 +87,18 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien.
+        # Make an alien and keep adding aliens untile there's no room left.
+        # Spacing between aliens is one alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
