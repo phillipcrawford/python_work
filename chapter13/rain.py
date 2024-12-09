307c2ff17a9472efacd263f46832145bc463e441
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-09 15:58:09 -0700

    create many drops

--- chapter13/rain.py
+++ chapter13/rain.py
@@ -16,21 +16,17 @@ class Rain:
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self)
-        self.bullets = pygame.sprite.Group()
-        self.aliens = pygame.sprite.Group()
+        pygame.display.set_caption("Rain")
+        self.rain = pygame.sprite.Group()
         #self.gnu = Gnu(self)
 
-        self._create_fleet()
+        self._create_drops()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()  
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            self._update_drops()
             self._update_screen()
             self.clock.tick(60)
 
@@ -41,92 +37,59 @@ class Rain:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
-        elif event.key == pygame.K_SPACE:
-            self._fire_bullet()
+        if event.key == pygame.K_q:
+            sys.exit()      
 
-    def _check_keyup_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-
-    def _fire_bullet(self):
-        """Create a new bullet and add it to the bullets group."""
-        if len(self.bullets) < self.settings.bullets_allowed:
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
-
-    def _update_bullets(self):
-        """Update position of bullets and get rid of old."""
-        # Update bullet position.
-        self.bullets.update()
-        # Get rid of bullets that have passed the top of screen
-        for bullet in self.bullets.copy():
-            if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet)        
-
-    def _update_aliens(self):
+    def _update_drops(self):
         """Update the positions of all aliens in the fleet."""
-        self._check_fleet_edges()
-        self.aliens.update()
+        # self._check_fleet_edges()
+        self.rain.update()
 
-    def _create_fleet(self):
+    def _create_drops(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens untile there's no room left.
         # Spacing between aliens is one alien width and one alien height.
-        alien = Alien(self)
-        alien_width, alien_height = alien.rect.size
+        drop = Drop(self)
+        drop_width, drop_height = drop.rect.size
 
-        current_x, current_y = alien_width, alien_height
-        while current_y < (self.settings.screen_height - 3 * alien_height):    
-            while current_x < (self.settings.screen_width - 2 * alien_width):
-                self._create_alien(current_x, current_y)
-                current_x += 2 * alien_width
+        current_x, current_y = drop_width, drop_height
+        while current_y < (self.settings.screen_height - drop_height):    
+            while current_x < (self.settings.screen_width - 2 * drop_width):
+                self._create_drop(current_x, current_y)
+                current_x += 2 * drop_width
 
             # Finished a row; reset x value, and increment y value.
-            current_x = alien_width
-            current_y += 2 * alien_height
+            current_x = drop_width
+            current_y += 2 * drop_height
 
-    def _create_alien(self, x_position, y_position):
+    def _create_drop(self, x_position, y_position):
         """Create an alien and place it in the row."""
-        new_alien = Alien(self)
+        new_alien = Drop(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
-        self.aliens.add(new_alien)
+        self.rain.add(new_alien)
 
-    def _check_fleet_edges(self):
-        """Respond appropriately if any aliens have reached an edge."""
-        for alien in self.aliens.sprites():
-            if alien.check_edges():
-                self._change_fleet_direction()
-                break
+#    def _check_fleet_edges(self):
+ #       """Respond appropriately if any aliens have reached an edge."""
+  #      for alien in self.aliens.sprites():
+   #         if alien.check_edges():
+    #            self._change_fleet_direction()
+     #           break
 
-    def _change_fleet_direction(self):
-        """Drop the entire fleet and change fleet's direction"""
-        for alien in self.aliens.sprites():
-            alien.rect.y += self.settings.fleet_drop_speed
-        self.settings.fleet_direction *= -1
+#    def _change_fleet_direction(self):
+ #       """Drop the entire fleet and change fleet's direction"""
+  #      for alien in self.aliens.sprites():
+   #         alien.rect.y += self.settings.fleet_drop_speed
+    #    self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
-        for bullet in self.bullets.sprites():
-            bullet.draw_bullet()
-        self.ship.blitme()
-        self.aliens.draw(self.screen)
-        #self.gnu.blitme()
+        self.rain.draw(self.screen)
 
         pygame.display.flip()
                     
