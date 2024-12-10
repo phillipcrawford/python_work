60eb1cd2c5f32a0a6051fbdbdb6bfa31bbc5bf54
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-10 13:59:51 -0700

    initialize game stats

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -21,6 +21,10 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
