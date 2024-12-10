2ab60d5f5644e1a06fc6cdd69bb69871b45fb4c7
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-10 08:44:48 -0700

    bullets destroy aliens

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -77,7 +77,12 @@ class AlienInvasion:
         # Get rid of bullets that have passed the top of screen
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet)        
+                self.bullets.remove(bullet)
+
+        # Check for any bullets that have hit aliens.
+        #   If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
