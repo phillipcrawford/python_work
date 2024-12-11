90d86ef9d42bf495471aedf07052e720b312d3b4
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-11 07:57:28 -0700

    complete exercise 13.6, game over

--- chapter12/sideways_shooter/settings.py
+++ chapter12/sideways_shooter/settings.py
@@ -7,7 +7,10 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        
+        # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 2.0
