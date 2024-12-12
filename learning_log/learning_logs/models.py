3bcabcc54fcf6e0fb5f92e76d7a34e25ec9c6077
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-12 10:30:05 -0700

    rename Topics to Topic, import Topic into admin

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 
 # Create your models here.
-class Topics(models.Model):
+class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
