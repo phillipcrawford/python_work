2ef4e5d0fea2bd6e2484436b89fd3ba6250d7686
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-13 11:52:49 -0700

    complete exercise 18.4, pizzeria

--- chapter18/pizzeria/pizzeria/models.py
+++ chapter18/pizzeria/pizzeria/models.py
@@ -7,4 +7,17 @@ class Pizza(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.text
+        return self.name
+    
+class Topping(models.Model):
+    """A Topping to be put on the pizza"""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def _str__(self):
+        """Return a string representation of the model."""
+        return f"{self.name[:50]}"
