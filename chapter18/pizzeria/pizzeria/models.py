911c0577230a5ddabd3a229a80dbdc364fbb22d9
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-13 11:27:02 -0700

    pizza model

---
+++ chapter18/pizzeria/pizzeria/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Pizza(models.Model):
+    """A Pizza the user is ordering"""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
