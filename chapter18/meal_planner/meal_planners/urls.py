ed97ddca6ef61e4b558a226051387c3b0b7aedf2
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-13 14:01:49 -0700

    add view and template, added app to settings

--- chapter18/meal_planner/meal_planners/urls.py
+++ chapter18/meal_planner/meal_planners/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'meal_planners'
 urlpatterns = [
     # Home page
     path('', views.index, name='index')
--- chapter18/meal_planner/meal_planners/views.py
+++ chapter18/meal_planner/meal_planners/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """The home page for Meal Planners."""
+    return render(request, 'meal_planners/index.html')
