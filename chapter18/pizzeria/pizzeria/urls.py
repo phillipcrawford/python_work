940327f4b37db3bd56db0e10fe565e81a244070e
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-14 12:06:22 -0700

    pizzas url

--- chapter18/pizzeria/pizzeria/urls.py
+++ chapter18/pizzeria/pizzeria/urls.py
@@ -8,4 +8,6 @@ app_name = 'pizzeria'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all topic.
+    path('pizzas/', views.pizzas, name='pizzas')
 ]
