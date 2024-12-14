b30a0760cbae32a18ca9a8097742358aa2372341
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-14 12:36:17 -0700

    pizza view

--- chapter18/pizzeria/pizzeria/views.py
+++ chapter18/pizzeria/pizzeria/views.py
@@ -11,3 +11,10 @@ def pizzas(request):
     pizzas = Pizza.objects.order_by('date_added')
     context = {'pizzas': pizzas}
     return render(request, 'pizzeria/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    """Show a single pizza and all its toppings"""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = Pizza.topping_set.order_by('-date_added')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzeria/pizza.html', context)
