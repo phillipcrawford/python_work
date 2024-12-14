27dfa9e62685436207f64535e4c4ce19fa03da47
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-14 13:01:12 -0700

    pizza template

--- chapter18/pizzeria/pizzeria/views.py
+++ chapter18/pizzeria/pizzeria/views.py
@@ -15,6 +15,6 @@ def pizzas(request):
 def pizza(request, pizza_id):
     """Show a single pizza and all its toppings"""
     pizza = Pizza.objects.get(id=pizza_id)
-    toppings = Pizza.topping_set.order_by('-date_added')
+    toppings = pizza.topping_set.order_by('-date_added')
     context = {'pizza': pizza, 'toppings': toppings}
     return render(request, 'pizzeria/pizza.html', context)
