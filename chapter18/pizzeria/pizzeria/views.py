de357c8b904aac3967d03512bf5bdcbaeca9cc12
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-14 12:15:44 -0700

    pizzas view

--- chapter18/pizzeria/pizzeria/views.py
+++ chapter18/pizzeria/pizzeria/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Pizza
 
 def index(request):
     """The home page for Pizzeria."""
     return render(request, 'pizzeria/index.html')
+
+def pizzas(request):
+    """Show all pizzas."""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzeria/pizzas.html', context)
