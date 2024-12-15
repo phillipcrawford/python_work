9c1fb93d383bbe0cd5bce479aa77706ab80cd7df
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-15 15:14:16 -0700

    debug topicform and entryform

--- learning_log/learning_logs/forms.py
+++ learning_log/learning_logs/forms.py
@@ -7,4 +7,10 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}        
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
