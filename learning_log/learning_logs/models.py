e6a07a37645d64cef0e8202ae8c0e30fe8cf7689
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-15 17:08:40 -0700

    remove redundant import statement in models

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
-# Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
