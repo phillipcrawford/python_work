22fc273b0e98f9bb85d3760cc38bec4ac9ba946c
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-17 10:44:40 -0700

    add all paths at once

--- chapter19/blog/blogs/urls.py
+++ chapter19/blog/blogs/urls.py
@@ -7,5 +7,15 @@ from . import views
 app_name = 'blogs'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('blogs/', views.blogs, name='blogs'),
+    # Detail page for a single topic.
+    path('blogs/<int:topic_id>/', views.blog, name='blog'),
+    # Page for adding a new topic.
+    path('new_blog/', views.new_blog, name='new_blog'),
+    # Page for adding a new entry.
+    path('new_blog_post/<int:topic_id>/', views.new_blog_post, name='new_blog_post'),
+    # Page for editing an entry.
+    path('edit_blog_post/<int:entry_id>/', views.edit_blog_post, name='edit_blog_post'),
 ]
